5763aea008f2ad1c8b4614861f6e655e9a9e54f3
Syed israr <121382749+syed1israr@users.noreply.github.com>
2024-09-09 19:38:41 +0530

    Update Cycel_detection_using_DFS.cpp

--- Cycel_detection_using_DFS.cpp
+++ Cycel_detection_using_DFS.cpp
@@ -2,69 +2,101 @@
 using namespace std;
 
 #define pii pair<int, int>
-#define inp(arr, n) for(int i = 0; i < n; i++) cin >> arr[i];
+#define inp(arr, n) for (int i = 0; i < n; i++) cin >> arr[i];
 #define prr(v) for (const auto& elem : (v)) cout << elem << " "; cout << endl;
 
-int n, m;
-vector<vector<int>> g;
-vector<int> col, parent;
-bool is_cycle_present = false;
-vector<int> any_Cycle;
+class GraphCycleDetector {
+private:
+    int n, m;
+    vector<vector<int>> adjList;
+    vector<int> color, parent, cycleNodes, cycleCount, prefixOrder;
+    bool cycleDetected = false;
 
-void dfs(int node, int par) {
-    parent[node] = par;
-    col[node] = 2;  // visiting
-    
-    for (auto v : g[node]) {
-        if (col[v] == 1) {
-            dfs(v, node);
-        } else if (col[v] == 2 && v != par) {
-            cout << "Hit : " << node << endl;
-            if (!is_cycle_present) {
-                int temp = node;
-                while (temp != v) {
-                    any_Cycle.push_back(temp);
-                    temp = parent[temp];
+    void dfs(int node, int par) {
+        parent[node] = par;
+        color[node] = 2;  // Mark as visiting
+
+        for (int neighbor : adjList[node]) {
+            if (color[neighbor] == 1) {
+                dfs(neighbor, node);
+            } else if (color[neighbor] == 2 && neighbor != par) {
+                // Cycle detected
+                if (!cycleDetected) {
+                    int temp = node;
+                    while (temp != neighbor) {
+                        cycleNodes.push_back(temp);
+                        temp = parent[temp];
+                    }
+                    cycleNodes.push_back(temp);
+                    reverse(cycleNodes.begin(), cycleNodes.end());
                 }
-                any_Cycle.push_back(temp);
-                reverse(any_Cycle.begin(), any_Cycle.end());
+                cycleCount[node]++;
+                cycleCount[parent[neighbor]]--;
+                cycleDetected = true;
             }
-            is_cycle_present = true;
         }
+        color[node] = 3;  // Mark as visited
+        prefixOrder.push_back(node);
     }
-    col[node] = 3;  // visited
-}
 
-void find_cycle_in_graph() {
-    cin >> n >> m;
-    g.resize(n + 1);
-    col.assign(n + 1, 1);  // unvisited
-    parent.assign(n + 1, 0);
-    
-    for (int i = 0; i < m; i++) {
-        int x, y;
-        cin >> x >> y;
-        g[x].push_back(y);
-    }
-    
-    for (int i = 1; i <= n; i++) {
-        if (col[i] == 1) {
-            dfs(i, 0);
+    void processCycleCounts() {
+        for (int node : prefixOrder) {
+            cycleCount[parent[node]] += cycleCount[node];
         }
     }
-    
-    if (is_cycle_present) {
-        prr(any_Cycle);
-    } else {
-        cout << "No cycle found\n";
+
+    void countCycleNodes() {
+        int cycleNodeCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (cycleCount[i] > 0) cycleNodeCount++;
+        }
+        cout << cycleNodeCount << endl;
     }
-}
+
+public:
+    void initializeGraph() {
+        cin >> n >> m;
+        adjList.resize(n + 1);
+        color.assign(n + 1, 1);  // Unvisited
+        parent.assign(n + 1, 0);
+        cycleCount.assign(n + 1, 0);
+
+        for (int i = 0; i < m; i++) {
+            int x, y;
+            cin >> x >> y;
+            adjList[x].push_back(y);
+        }
+    }
+
+    void detectCycle() {
+        for (int i = 1; i <= n; i++) {
+            if (color[i] == 1) {
+                dfs(i, 0);
+            }
+        }
+    }
+
+    void reportResults() {
+        if (cycleDetected) {
+            prr(cycleNodes);
+        } else {
+            cout << "No cycle found\n";
+        }
+
+        processCycleCounts();
+        countCycleNodes();
+    }
+};
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    
-    find_cycle_in_graph();
+
+    GraphCycleDetector detector;
+    detector.initializeGraph();
+    detector.detectCycle();
+    detector.reportResults();
+
     return 0;
 }
